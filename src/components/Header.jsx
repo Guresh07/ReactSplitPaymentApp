08ed08de7a050f47c8c642cb96e637d232dcd296
Guresh07 <challaguresh1234@gmail.com>
2025-08-07 15:58:40 +0530

    header fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,7 +23,7 @@ function Header() {
                     </div>
                     {/* <button className="btn btn-primary" onClick={handleLogout}>Logout</button> */}
                     <div className="userIconContainer d-flex justify-content-center align-items-center rounded-circle p-3"
-                        style={{ "backgroundColor": "rgb(219 220 244)",cursor: "pointer" }}
+                        style={{ "backgroundColor": "rgb(219 220 244)", cursor: "pointer" }}
                         data-bs-toggle="modal"
                         data-bs-target="#profileModal"
                         title='UserProfile'
@@ -31,42 +31,42 @@ function Header() {
                         <i className="fa-solid fa-user" style={{ "color": "#0049c7" }}></i>
                     </div>
                     {/* Profile Modal */}
-                    <div className="modal fade" id="profileModal" tabIndex="-1" aria-hidden="true">
-                        <div className="modal-dialog modal-dialog-centered">
-                            <div className="modal-content">
-
-                                <div className="modal-header">
-                                    <h5 className="modal-title">User Profile</h5>
-                                    <button type="button" className="btn-close" data-bs-dismiss="modal"></button>
-                                </div>
-
-                                <div className="modal-body">
-                                    {currentUser ? (
-                                        <>
-                                            <p><strong>Username:</strong> {currentUser.userData.userName}</p>
-                                            <p><strong>Email:</strong> {currentUser.userData.email}</p>
-                                            <button
-                                                className="btn btn-danger w-100"
-                                                onClick={handleLogout}
-                                                data-bs-dismiss="modal"
-                                            >
-                                                Sign Out
-                                            </button>
-                                        </>
-                                    ) : (
-                                        <>
-                                            <button className="btn btn-primary w-100 mb-2">Sign In</button>
-                                            <button className="btn btn-outline-primary w-100">Sign Up</button>
-                                        </>
-                                    )}
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
 
                 </div>
             </header>
+            <div className="modal fade" id="profileModal" tabIndex="-1" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered">
+                    <div className="modal-content">
+
+                        <div className="modal-header">
+                            <h5 className="modal-title">User Profile</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal"></button>
+                        </div>
+
+                        <div className="modal-body">
+                            {currentUser ? (
+                                <>
+                                    <p><strong>Username:</strong> {currentUser.userData.userName}</p>
+                                    <p><strong>Email:</strong> {currentUser.userData.email}</p>
+                                    <button
+                                        className="btn btn-danger w-100"
+                                        onClick={handleLogout}
+                                        data-bs-dismiss="modal"
+                                    >
+                                        Sign Out
+                                    </button>
+                                </>
+                            ) : (
+                                <>
+                                    <button className="btn btn-primary w-100 mb-2">Sign In</button>
+                                    <button className="btn btn-outline-primary w-100">Sign Up</button>
+                                </>
+                            )}
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
